9f1bd456c1baccf2e5b4c723a664cb0ed419cddd
Simon T <simon.thouet@gmail.com>
2025-10-20 18:11:58 +0200

    init project

---
+++ tbs-back/src/main/java/com/tbs/travel_buddy_split/TravelBuddySplitApplication.java
@@ -0,0 +1,13 @@
+package com.tbs.travel_buddy_split;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBuddySplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBuddySplitApplication.class, args);
+	}
+
+}
---
+++ tbs-back/src/test/java/com/tbs/travel_buddy_split/TravelBuddySplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbs.travel_buddy_split;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBuddySplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
